0266a37303d43d378158521870a917c0e135cbd7
YoelZalas <yoel.zalas@gmail.com>
2016-03-28 16:39:49 -0500

    bower.json added

---
+++ js/ngapp.js
@@ -0,0 +1,8 @@
+angular.module('ngApp', [])
+.controller('ngCtrl', function( $scope ){
+	$scope.sendMessage = function ( ) {
+		// console.log('data to send', $scope.messageForm);
+		$scope.myForm
+	}
+})
+;
